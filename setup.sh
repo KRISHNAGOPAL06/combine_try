8f411857df3bd229d48ff7e7278b279f77379781
KRISHNAGOPAL06 <70538457+KRISHNAGOPAL06@users.noreply.github.com>
2024-02-16 18:07:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 port = $PORT\n\
-enableCORS = true\n\
+enableCORS = false\n\
 headless = true\n\
 allowAllOrigins = true\n\
 " > ~/.streamlit/config.toml
