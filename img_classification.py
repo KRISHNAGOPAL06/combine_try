4c5cda85bc1ae01dbff949512884329b43f357c7
KRISHNAGOPAL06 <70538457+KRISHNAGOPAL06@users.noreply.github.com>
2024-02-16 12:14:07 +0530

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from keras.preprocessing.image import load_img,img_to_array
 import tensorflow as tf
+import io
 
 import numpy as np
 import keras
@@ -23,17 +24,28 @@ def teachable_machine_classification(img, weights_file):
     
     model = keras.models.load_model(weights_file)
 
-    # Load the image
-    image = tf.keras.preprocessing.image.load_img(img, target_size=(224, 224))
+    # Convert PIL image to bytes
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='JPEG')
+    img_bytes.seek(0)
+
+    # Load the image using load_img
+    image = tf.keras.preprocessing.image.load_img(img_bytes, target_size=(224, 224))
+    
     # Convert image to numpy array
     img_array = tf.keras.preprocessing.image.img_to_array(image)
+    
     # Expand dimensions to match model input shape
     img_array = np.expand_dims(img_array, axis=0)
+    
     # Make prediction
     prediction = model.predict(img_array)
+    
     # Get predicted class index
     predicted_class_index = np.argmax(prediction)
+    
     # Get class label corresponding to the predicted index
     predicted_class_label = classes[predicted_class_index]
+    
     return predicted_class_label
 
