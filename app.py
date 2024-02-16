41f0cfbc9a62443dbdec3918f7b7459c30d6a249
KRISHNAGOPAL06 <70538457+KRISHNAGOPAL06@users.noreply.github.com>
2024-02-16 12:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from img_classification import teachable_machine_classification
 from PIL import Image
 import numpy as np
 import io
+from flask_cors import CORS
 
 st.title("Python or Anaconda Predictor")
 st.header("Large Serpent Classifier")
@@ -12,6 +13,9 @@ st.text("Upload an Image for image classification as Anaconda or Python")
 def classify_image(image_data):
     image = Image.open(io.BytesIO(image_data))
     return teachable_machine_classification(image, 'trained_model_accurate.h5')
+
+# Allow requests from all origins
+CORS(app)
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
