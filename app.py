4ba99a2d7d72852375572c02a221adab7ab4ab2e
KRISHNAGOPAL06 <70538457+KRISHNAGOPAL06@users.noreply.github.com>
2024-02-20 09:33:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
-
+classes_list=['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'invalid']
+model_name='apple_last.h5'
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -18,25 +23,11 @@ def predict():
             return jsonify({'error': 'No selected file'}), 400
 
         image = Image.open(file)
-        label = teachable_machine_classification(image, 'trained_model_accurate.h5')
+        label = teachable_machine_classification(image, model_name,classes_list)
         return jsonify({'label': label}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-@app.route('/apple_predict', methods=['POST'])
-def predict():
-    try:
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file part'}), 400
 
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
-
-        image = Image.open(file)
-        label = apple_classification(image, 'apple_last.h5')
-        return jsonify({'label': label}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
