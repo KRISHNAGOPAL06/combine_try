5dd191bd61106fa7fa9f49026c1432300f069dd6
KRISHNAGOPAL06 <70538457+KRISHNAGOPAL06@users.noreply.github.com>
2024-02-20 21:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 import requests
-from io import BytesIO\
+from io import BytesIO
 from apple import apple_classification
 
 app = Flask(__name__)
