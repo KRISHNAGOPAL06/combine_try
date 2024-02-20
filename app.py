d0eba00f21de079c01210f7768fccf4c223a06db
KRISHNAGOPAL06 <70538457+KRISHNAGOPAL06@users.noreply.github.com>
2024-02-20 10:48:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ from PIL import Image
 app = Flask(__name__)
 CORS(app)
 
+# Define a dictionary to store model names and classes lists for different crops
+crop_models = {
+    'apple': {
+        'classes_list': ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'invalid'],
+        'model_name': 'apple_last.h5'
+    },
+    # Add more crops and their corresponding model names and classes lists here
+}
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -16,9 +24,14 @@ def predict():
 
         file = request.files['file']
         crop_name = request.form.get('crop_name')  # Get crop name from form data
-        if crop_name == 'apple':
-         classes_list=['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy','invalid']
-         model_name='apple_last.h5'
+        
+        if crop_name not in crop_models:
+            return jsonify({'error': 'Invalid crop name'}), 400
+
+        model_info = crop_models[crop_name]
+        classes_list = model_info['classes_list']
+        model_name = model_info['model_name']
+
         if file.filename == '':
             return jsonify({'error': 'No selected file'}), 400
         
@@ -27,7 +40,6 @@ def predict():
         return jsonify({'label': label}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
